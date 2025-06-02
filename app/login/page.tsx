2124445466bd38ef01599fa2eeacde8152b507cc
septemwee <septemwee@gmail.com>
2025-06-02 16:13:14 +0700

    mawawwww

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,3 @@
-import { loginAction } from '@/actions/login.action'
 import { LoginForm } from '@/app/components/ui/loginForm'
 
 export default function LoginPage() {
@@ -21,7 +20,7 @@ export default function LoginPage() {
           }}>
         </div> 
         <div className="min-h-screen flex items-center justify-center bg-[#EEEEEE]">
-          <LoginForm action={loginAction} />
+          <LoginForm />
         </div>
       </div>
      
