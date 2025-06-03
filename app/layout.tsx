518313eae8c9500cc4c1c8a1b19db5e7f27a61eb
MeepoohCH <pooh90900@gmail.com>
2025-06-03 15:57:18 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import {Kanit } from "next/font/google";
 import "./globals.css";
 import SideNavbar from "./components/ui/SideNavbar";
 
@@ -9,16 +8,6 @@ const kanit = Kanit({
   variable: "--font-kanit", // ใช้ร่วมกับ Tailwind ได้
 });
 
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -48,7 +37,3 @@ export default function RootLayout({
     </html>
   )
 }
-
-
-
-
