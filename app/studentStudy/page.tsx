10df23b802c3d3e588845eaa59d221b1f69a20b2
MeepoohCH <pooh90900@gmail.com>
2025-06-05 16:36:47 +0700

    Connect database

--- app/studentStudy/page.tsx
+++ app/studentStudy/page.tsx
@@ -1,23 +1,5 @@
-import ScheduleTable from '@/app/components/ui/ScheduleTable'
 import StudentDropdown from '../components/ui/studentDropdown';
 import StudyForm from '../components/StudyForm';
-
-const mockClasses = [
-  {
-    day: 'จันทร์',
-    starttime: "08:45",
-    endtime: "10:15",
-    subject: 'CALCULUS1',
-    subjectid: '01006030 (ท) ปี 1 กลุ่ม 4',
-  },
-  {
-    day: 'อังคาร',
-    starttime: "13:00",
-    endtime: "20:00",
-    subject: 'PHYSICS1',
-    subjectid: '02007040 (ท) ปี 2 กลุ่ม 1',
-  },
-];
 
 
 export default function SchedulePage() {
