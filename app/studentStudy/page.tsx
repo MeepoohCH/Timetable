dcd64aba5b72992ce8d7f6cfea69cc34a9296a90
MeepoohCH <pooh90900@gmail.com>
2025-06-09 14:32:23 +0700

    delete

--- app/studentStudy/page.tsx
+++ app/studentStudy/page.tsx
@@ -1,6 +1,6 @@
 import StudentDropdown from '../components/ui/studentDropdown';
 import StudyForm from '../components/StudyForm';
-import ExamForm from '../Exam/page';
+
 
 
 export default function SchedulePage() {
@@ -10,7 +10,7 @@ export default function SchedulePage() {
       <div className="px-4 sm:px-6 py-4 sm:py-6 overflow-x-auto bg-[#EFEFEF] w-full">
         <h1 className="text-2xl font-medium mb-4">ตารางนักศึกษา</h1>
         <div className="mb-6">
-          <StudentDropdown />o
+          <StudentDropdown />
           <StudyForm/>
         </div>
       </div>
