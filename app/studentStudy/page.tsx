98ca74fcffec391803952a6dd2272278184f02c8
MeepoohCH <pooh90900@gmail.com>
2025-06-09 14:11:27 +0700

    welcome

--- app/studentStudy/page.tsx
+++ app/studentStudy/page.tsx
@@ -10,7 +10,7 @@ export default function SchedulePage() {
       <div className="px-4 sm:px-6 py-4 sm:py-6 overflow-x-auto bg-[#EFEFEF] w-full">
         <h1 className="text-2xl font-medium mb-4">ตารางนักศึกษา</h1>
         <div className="mb-6">
-          <StudentDropdown />
+          <StudentDropdown />o
           <StudyForm/>
         </div>
       </div>
